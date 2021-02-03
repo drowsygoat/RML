8a4e9ac4238842fb7b1244baead1a98cee81c76f
drowsygoat <lecka48@liu.se>
2021-02-03 01:06:09 +0100

    diamond_with_filter_networks.sh to 12_dec

---
+++ cluster_runs/12_dec/diamond_with_filtered_networks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sbatch <<EOT
+#!/bin/sh
+
+#SBATCH -A snic2020-15-75
+#SBATCH -J $1 # name
+#SBATCH -o $1_%j.out
+#SBATCH -t $2 # time
+#SBATCH -e $1\_%j.out
+#SBATCH -p $3 # core or devel
+#SBATCH -n $4 # cores
+#SBATCH --mail-user=lecka48@liu.se
+#SBATCH --mail-type=ALL
+
+module purge
+module load R/4.0.0
+module --ignore-cache load gcc/10.1.0
+module load openmpi/3.1.6
+mpirun -n $4 R --no-save --args $5 $6 < 12_dec_diamond_network_filtration.R
+EOT
