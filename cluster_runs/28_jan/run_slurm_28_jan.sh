49774a4e7c72b25d2072ee217247e93eed6a77ba
drowsygoat <lecka48@liu.se>
2021-02-15 14:48:52 +0100

    modification of script

--- cluster_runs/28_jan/run_slurm_28_jan.sh
+++ cluster_runs/28_jan/run_slurm_28_jan.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 sbatch <<EOT
 #!/bin/sh
-
 #SBATCH -A snic2020-15-75
-#SBATCH -J 12_dec_dist
-#SBATCH -o 12_dec_dist_%j.out
-#SBATCH -t 01:00:00
-#SBATCH -e 12_dec_dist_%j.out
-#SBATCH -p devel
-#SBATCH -n 10
+#SBATCH -J $1 # name
+#SBATCH -o $1_%j.out
+#SBATCH -t $2 # time
+#SBATCH -e $1\_%j.out
+#SBATCH -p $3 # core or devel
+#SBATCH -n $4 # cores
 #SBATCH --mail-user=lecka48@liu.se
 #SBATCH --mail-type=ALL
 
@@ -16,5 +15,6 @@ module purge
 module load R/4.0.0
 module --ignore-cache load gcc/10.1.0
 module load openmpi/3.1.6
-mpirun -n 10 R --no-save --args $1 $2 < 12_dec_distances.R
+mpirun -n $4 R --no-save --args $6 $7 < $5  # 5 is script name, 6 is alpha, 7 is iterations,
+
 EOT
